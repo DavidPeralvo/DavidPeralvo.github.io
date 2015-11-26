40c96e3a544a583a661dcb0f1f92502cc535e61e
David Peralvo <davidperalvo@gmail.com>
2015-11-26 10:18:21 +0100

    Subida array matematicos

--- ejercicios/prototype/js/arrayMatematicos.js
+++ ejercicios/prototype/js/arrayMatematicos.js
@@ -1,16 +1,16 @@
 /**
  * Created by DavidPeralvoGomez on 24/11/15.
  */
-function arrayMatematicos(filas,columnas){
-    this.filas=filas;
-    this.columnas=columnas;
-    this.contenido=new Array();
+function ArrayMatematicos(filas,columnas){
+    this.filas=document.getElementById("filas").value;
+    this.columnas=document.getElementById("columnas").value;
+    this.contenido=this.crearArray();
 }
 
-arrayMatematicos.prototype.sumar=function (array2){
+ArrayMatematicos.prototype.sumar=function (array2){
     var suma;
     if(this.coincideMatriz(array2)) {
-        suma=new arrayMatematicos(this.filas,this.columnas);
+        suma=new ArrayMatematicos(this.filas,this.columnas);
         for (var i = 0; i < this.filas; i++) {
             for (var j = 0; j < this.columnas; j++) {
                 suma.contenido[i][j]= this.contenido[i][j] + array2.contenido[i][j];
@@ -19,32 +19,42 @@ arrayMatematicos.prototype.sumar=function (array2){
         return suma;
     }
     else{
-        //mostrar mensaje
+        contendor.innerHTML="Las matrices no son iguales";
     }
 
 }
 
-arrayMatematicos.prototype.restar=function(array2){
+ArrayMatematicos.prototype.restar=function(array2) {
     var resta;
-    if(this.coincideMatriz(array2)){
-       resta=new arrayMatematicos(this.filas,this.columnas);
-        for(var i=0;i<this.filas;i++){
-            for(var j=0;j<this.columnas;j++){
-                resta.contenido[i][j]=this.contenido[i][j] - array2.contenido[i][j];
+    if (this.coincideMatriz(array2)) {
+        resta = new ArrayMatematicos(this.filas, this.columnas);
+        for (var i = 0; i < this.filas; i++) {
+            for (var j = 0; j < this.columnas; j++) {
+                resta.contenido[i][j] = this.contenido[i][j] - array2.contenido[i][j];
             }
         }
         return resta;
     }
-    else{
-        //mensaje de error
+    else {
+        contendor.innerHTML = "Las matrices no son iguales";
     }
+}
+ArrayMatematicos.prototype.multiplicar=function(array2){
+    var multiplicar;
+    if(this.coincideMatriz(array2)){
+        multiplicar=new ArrayMatematicos(this.filas,this.columnas);
 
-
-
+    }
+    else{
+        contendor.innerHTML = "Las matrices no son iguales";
+    }
 }
 
-arrayMatematicos.prototype.coincideMatriz=function(array2){
-    if(this.filas===array2.filas && this.columnas===array2.columnas){
+
+
+
+ArrayMatematicos.prototype.coincideMatriz=function(array2){
+    if(this.filas === array2.filas && this.columnas === array2.columnas){
         return true;
     }
     else{
@@ -53,35 +63,91 @@ arrayMatematicos.prototype.coincideMatriz=function(array2){
 
 }
 
-function inicio(){
-
-    var botonmatriz=document.getElementById("boton");
-    botonmatriz.onclick=crearMatriz;
-
-
-
-}
-function crearMatriz(){
-
-    var filas=document.getElementById("filas").value;
-    var columnas=document.getElementById("columnas").value;
-    var contenedor=document.getElementById("contenido");
-
-
-    for(var i=0;i<filas;i++){
-        for(var j=0;j<columnas;j++){
-            contenedor.innerHTML+="n"+(i+1)+(j+1)+":<input type='text' name='valor' id='"+(i+1)+(j+1)+"+'/>";
-
+ArrayMatematicos.prototype.mostrarMatriz= function() {
+    var matriz = "";
+    for (var i = 0; i < this.filas; i++) {
+        for (var j = 0; j < this.columnas; j++) {
+            matriz += this.contenido[i][j] + "	";
         }
-        contenedor.innerHTML+="<br>";
-
+        matriz += "<br/>";
     }
+    return matriz;
+}
 
-    contenedor.innerHTML+="<br><br><input type='button' name='calculo' value='Calcular' id='calcular'>";
+
+
+
+ArrayMatematicos.prototype.crearArray = function() {
+    var matriz = new Array();
+    for (var i = 0; i < this.filas; i++) {
+        matriz[i] = new Array();
+        for (var j = 0; j < this.columnas; j++)
+            matriz[i][j] = Math.round(Math.random()*10);
+    }
+    return matriz;
+}
+
+ArrayMatematicos.prototype.transpuesta=function(){
+    var matriz =new ArrayMatematicos(this.filas,this.columnas);
+    for (var i = 0; i < matriz.filas; i++) {
+        for (var j = 0; j < matriz.columnas; j++) {
+            matriz.contenido[i][j] = this.contenido[j][i];
+        }
+    }
+    return matriz;
+
+}
+
+function suma(){
+    var contenedor=document.getElementById("contenido");
+    var sumando1,sumando2,resultado;
+
+    sumando1=new ArrayMatematicos(filas,columnas);
+    sumando2=new ArrayMatematicos(filas,columnas);
+    resultado=sumando1.sumar(sumando2);
+    contenedor.innerHTML="";
+    contenedor.innerHTML+="Matriz 1 <br>"+sumando1.mostrarMatriz();
+    contenedor.innerHTML+="Matriz 2 <br>"+sumando2.mostrarMatriz();
+    contenedor.innerHTML+="Resultado <br>"+resultado.mostrarMatriz();
+
+
+
+
+}
+function resta(){
+    var contenedor=document.getElementById("contenido");
+    var sumando1,sumando2,resultado;
+
+    resta1=new ArrayMatematicos();
+    resta2=new ArrayMatematicos();
+    resultado=resta1.restar(resta2);
+    contenedor.innerHTML="";
+    contenedor.innerHTML+="Matriz 1 <br>"+resta1.mostrarMatriz();
+    contenedor.innerHTML+="Matriz 2 <br>"+resta2.mostrarMatriz();
+    contenedor.innerHTML+="Resultado <br>"+resultado.mostrarMatriz();
+
+
+
+
+}
+function transpuesta(){
+    var contenedor=document.getElementById("contenido");
+    var trans,resultado;
+    trans=new ArrayMatematicos();
+    resultado=trans.transpuesta();
+
+    contenedor.innerHTML="";
+    contenedor.innerHTML+="Matriz  <br>"+trans.mostrarMatriz();
+    contenedor.innerHTML+="Resultado <br>"+resultado.mostrarMatriz();
 
 
 
 }
 
 
-window.addEventListener("load",inicio);
+
+window.addEventListener('load', function(){
+    document.getElementById('sumar').addEventListener("click", suma);
+    document.getElementById('restar').addEventListener("click", resta);
+    document.getElementById('transpuesta').addEventListener("click", transpuesta);
+});
