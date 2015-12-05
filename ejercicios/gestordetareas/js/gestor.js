738d2c469a2fe7e1943b5a6265fb90ed1eea0882
David Peralvo <davidperalvo@gmail.com>
2015-12-05 12:17:25 +0100

    Subida gestor de tareas- Correcion gestor.js

--- ejercicios/gestordetareas/js/gestor.js
+++ ejercicios/gestordetareas/js/gestor.js
@@ -3,7 +3,7 @@
  */
 function carga() {
     setTimeout("document.getElementById('cargando').style. display = 'none'", 3000);
-
+    tipoError="";
 
 
 }
@@ -24,7 +24,7 @@ document.getElementById("enviar").onclick=function(){
      var descripcion=document.getElementById("contenido").value;
      var horario=document.getElementById("hora").value;
     var fecha=document.getElementById("fecha").value;
-    if(comprobarFecha(fecha) && comprobarHora(horario)) {
+    if(comprobarTitulo(titulo) && comprobarModulo(modulo) && comprobarDescripcion(descripcion) && comprobarFecha(fecha) && comprobarHora(horario)) {
         var fechaEntrega=fecha+" - "+horario;
         document.getElementById("actividad").innerHTML += "<br><article>" + titulo + "<hr><h3>Modulo:</h3>"+modulo+"<h3>Tarea:</h3>" + descripcion + "<br><h3>Fecha Entega:</h3>" + fechaEntrega + "</article>";
 
@@ -40,13 +40,7 @@ document.getElementById("enviar").onclick=function(){
         document.getElementById("boton").style.display='none';
         error.style.display="block";
         error.innerHTML="";
-        var tipoError="";
-        if(comprobarFecha(fecha)){
-          tipoError="Formato de hora o hora introducida incorrecta";
-        }
-        else{
-            tipoError="Formato de fecha o fecha introducida incorrecta";
-        }
+
         error.innerHTML="<div id='error'>"+tipoError+"<br><br> <input type='button' value='Aceptar'id='aceptar'></div>";
         document.getElementById("aceptar").onclick=function(){
             error.style.display="none";
@@ -62,14 +56,50 @@ document.getElementById("cancelar").onclick=function(){
     document.getElementById("formulario").style.display='none';
     document.getElementById("boton").style.display='block';
 }
+function comprobarTitulo(titulo){
+    if(titulo==""){
+        tipoError="Por favor introduzca el nombre de la tarea ";
+        return false;
+    }
+    else{ return true;}
+}
+function comprobarModulo(modulo){
+    if(modulo==""){
+        tipoError="Por favor introduzca el modulo de la tarea ";
+        return false;
+    }
+    else{ return true;}
+}
+function comprobarDescripcion(descripcion){
+    if(titulo==""){
+        tipoError="Por favor introduzca la descripción de la tarea ";
+
+        return false;
+    }
+    else{ return true;}
+}
+
+
 function comprobarFecha(fecha){
     var ExpReg=/(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
-    return ExpReg.test(fecha);
+    if(ExpReg.test(fecha)){
+        return true;
+    }
+    else{
+        tipoError="Fecha mal introducida / Fecha no introducida"
+        return false
+    }
 }
 
 function  comprobarHora(hora){
     var ExpReg=/^[0-2][0-9]:[0-5][0-9]$/;
-    return ExpReg.test(hora);
+    if(ExpReg.test(hora)){
+        return true;
+    }
+    else{
+        tipoError="Hora mal introducida / Hora no introducida"
+        return false
+    }
 }
 
 
