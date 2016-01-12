d47b2c80391e95c40626789fd18fba8ee097661c
David Peralvo <davidperalvo@gmail.com>
2016-01-12 12:42:19 +0100

    correcion formulario completito

--- ejercicios/tema5/js/validacion.js
+++ ejercicios/tema5/js/validacion.js
@@ -16,71 +16,77 @@ window.addEventListener("load",function(){
     var url=document.getElementById('url');
     var condiciones=document.getElementById('condiciones');
     var enviar=document.getElementById('enviar');
-    document.formulario.enviar.disabled=true;
+    var error=document.getElementById('error');
+
+    var expresionNombre=new RegExp(/^[\D]+$/);
+    var expresionEmail=new RegExp(/[\w-]+(\.[\w-]+)*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/);
+    var expresionTelefono= new RegExp(/^\d{9}$/);
+    var expresionCuenta= new RegExp(/^\d{4}-\d{4}-\d{2}-\d{10}$/);
+    var expresionURL=new RegExp(/^(http|https|ftp)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi);
+
 
     nombre.focus();
     nombre.addEventListener("blur",function(){
-        if(validarTexto(nombre)){
-            nombre.style.border="1px solid green";}
-        else{nombre.style.border="1px solid red";}
+        if(validar(nombre,expresionNombre)){
+
+            nombre.style.border="1px solid green";apellido1.focus();}
+        else{nombre.style.border="1px solid red"; }
     });
-    apellido1.focus();
+
     apellido1.addEventListener("blur",function(){
-        if(validarTexto(apellido1)){apellido1.style.border="1px solid green";}
-        else{apellido2.style.border="1px solid red";}
+        if(validar(apellido1,expresionNombre)){apellido1.style.border="1px solid green";apellido2.focus();
+        } else{apellido1.style.border="1px solid red";}
     });
-    apellido2.focus();
+
     apellido2.addEventListener("blur",function(){
-        if(validarTexto(apellido2)){apellido2.style.border="1px solid green";
+        if(validar(apellido2,expresionNombre)){apellido2.style.border="1px solid green";email.focus();
         } else{apellido2.style.border="1px solid red";}
     });
-    email.focus();
+
     email.addEventListener("blur",function(){
-        if(validarEmail(email)){email.style.border="1px solid green";}
+        if(validar(email,expresionEmail)){email.style.border="1px solid green";dni.focus();}
         else{email.style.border="1px solid red";}
     });
-    dni.focus();
+
     dni.addEventListener("blur",function(){
-        if(validarDni(dni)){dni.style.border="1px solid green";}
+        if(validarDni(dni)){dni.style.border="1px solid green";fecha.focus();}
         else{dni.style.border="1px solid red";}
     });
-    fecha.focus();
+
     fecha.addEventListener("blur", function(){
-        if(validarFecha(fecha)){fecha.style.border="1px solid green";}
+        if(validarFecha(fecha)){fecha.style.border="1px solid green";telefono.focus();}
         else{fecha.style.border="1px solid red";}
     });
-    telefono.focus();
+
     telefono.addEventListener("blur",function(){
-        if(validarTelefono(telefono)){telefono.style.border="1px solid green";}
+        if(validar(telefono,expresionTelefono)){telefono.style.border="1px solid green";banco.focus();}
         else{
             telefono.style.border="1px solid red";
         }
     });
-    banco.focus();
+
     banco.addEventListener("blur",function(){
-        if(validarCuenta(banco)){banco.style.border="1px solid green";}
+        if(validar(banco,expresionCuenta)){banco.style.border="1px solid green";url.focus();}
         else{banco.style.border="1px solid red";}
     });
-    url.focus();
+
     url.addEventListener("blur",function(){
-        if(validarUrl(url)){url.style.border="1px solid green";}
+        if(validar(url,expresionURL)){url.style.border="1px solid green";condiciones.focus();}
         else{ url.style.border="1px solid red";}
     });
-    condiciones.focus();
-    condiciones.addEventListener("blur",function(){
-        if(validarCondiciones(condiciones)){
-        condiciones.style.border="1px solid green";
-            document.formulario.enviar.disabled=false;
+
+
+    enviar.addEventListener("click",function(){
+        if(validar(nombre,expresionNombre) && validar(apellido1,expresionNombre) && validar(apellido2,expresionNombre) && validar(email,expresionEmail) && validarDni(dni) && validarFecha(fecha) && validar(telefono,expresionTelefono) && validar(banco,expresionCuenta) && validar(url,expresionURL) && validarCondiciones(condiciones)){
+            var nuevaVentana=window.open();
+            nuevaVentana.document.open();
+            nuevaVentana.document.write("Formulario enviado");
+            error.innerHTML="";
         }
         else{
-        condiciones.style.border="1px solid red";
-            document.formulario.enviar.disabled=true;
+            error.innerHTML="Error en los campos";
         }
-    });
-    enviar.addEventListener("click",function(){
-    var nuevaVentana=window.open();
-    nuevaVentana.document.open();
-    nuevaVentana.document.write("Formulario enviado");
+
     });
 
 
@@ -88,24 +94,20 @@ window.addEventListener("load",function(){
 
 
 
+function validar(valor,expresion){
 
-function validarTexto(valor){
-    if(valor.value=="" || valor.value==null  ||(/^\d+$/.test(valor.value))){
-        valor.focus();
-        return false;}
+    if(valor.value!="" && valor.value!=null  && (expresion.test(valor.value))){
+        return true;}
     else{
-        return true;}
-}
-
-function validarEmail(valor){
-    if(/^[\w-]+(\.[\w-]+)*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/.test(valor.value)){
-        return true;}
-    else {
         valor.focus();
-        return false;}
+        return false;
+        }
+
+
 }
 
 function validarDni(valor){
+
     letrasDni=['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T']
     if((/^\d{8}[A-Z]$/.test(valor.value))){
         if(valor.value.charAt(8) == letrasDni[(valor.value.substring(0, 8))%23]){
@@ -119,8 +121,8 @@ function validarDni(valor){
 }
 
 function validarFecha(valor){
-
-    if(/^(\d{2})-(\d{2})-(\d{4})$/.test(valor.value)){
+    var expresionFecha=new RegExp(/^(\d{2})-(\d{2})-(\d{4})$/);
+    if(expresionFecha.test(valor.value)){
         var arrayfecha = valor.value.split("-");
         var dia = arrayfecha[0];
         var mes = arrayfecha[1];
@@ -135,37 +137,14 @@ function validarFecha(valor){
     }
     else{valor.focus();return false}
 }
-function validarTelefono(valor){
-    if(/\d{9}$/.test(valor.value)){
-        return true;
-    }
-    else{valor.focus();return false;}
-}
-function validarCuenta(valor){
-    if(/^\d{4}-\d{4}-\d{2}-\d{10}$/.test(valor.value)){
-        return true;
-    }
-    else{valor.focus();return false;
-    }
-}
-
-function validarUrl(valor){
-    if(/^(http|https|ftp)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi.test(valor.value)){
-        return true;
-    }
-    else{
-        valor.focus();
-        return false
-    }
-}
 
 function validarCondiciones(valor){
-    if(valor.checked){
-        return true;
+    if(!valor.checked){
+        return false;
 
     }
     else{
-        valor.focus();
-        return false;
+
+        return true;
     }
 }
