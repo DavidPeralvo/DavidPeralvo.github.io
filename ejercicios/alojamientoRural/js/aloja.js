80bd33a7fc93514acd7212d4641464eba453b9ad
David Peralvo <davidperalvo@gmail.com>
2016-01-19 20:32:53 +0100

    mejora alojamiento

---
+++ ejercicios/alojamientoRural/js/aloja.js
@@ -0,0 +1,10 @@
+/**
+ * Created by DavidPeralvoGomez on 19/1/16.
+ */
+
+
+    document.getElementsByName("reserva").onclick=function(){
+        document.getElementById("filtro").style.display='block'; };
+
+
+
