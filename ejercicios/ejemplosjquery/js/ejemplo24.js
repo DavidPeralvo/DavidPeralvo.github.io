f5ea0b59fec4cebc8be7019b6cf5582aa036c6fe
David Peralvo <davidperalvo@gmail.com>
2016-02-15 20:47:21 +0100

    commit

---
+++ ejercicios/ejemplosjquery/js/ejemplo24.js
@@ -0,0 +1,19 @@
+jQuery.fn.cuentaCaracteres = function() {
+    var elem;
+    this.each(function(){
+        elem = $(this);
+        var contador = $('<div>Contador caracteres: ' + elem.attr("value").length + '</div>');
+        elem.after(contador);
+        elem.data("campocontador", contador);
+        elem.keyup(function(){
+            var elem = $(this);
+            var campocontador = elem.data("campocontador");
+
+            campocontador.text('Contador caracteres: ' + elem.attr("value").length);
+        });
+    });
+    return this;
+};
+$(document).ready(function(){
+    $("textarea").cuentaCaracteres();
+});
