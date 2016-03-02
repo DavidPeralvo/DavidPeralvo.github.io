49c954493c026540d34f83676371ec5afee47676
David Peralvo <davidperalvo@gmail.com>
2016-03-02 18:19:27 +0100

    casarural efecto

--- ejercicios/casasruralesjquery/js/inicio.js
+++ ejercicios/casasruralesjquery/js/inicio.js
@@ -4,15 +4,23 @@ $(document).ready(function(){
     casa.delay(1000);
     casa.animate({top:'100%'},2000);
 
+
     var tractor=$('#tractor');
     tractor.delay(2000);
     tractor.animate({left:'100%'},2000);
 
 
 
+
     var  intro=$(".contenido");
-    intro.delay(1000);
+    intro.delay(1500);
     intro.hide();
     intro.delay(2000);
     intro.fadeIn(1500);
+
+    if(navigator.cookieEnabled==0){
+
+        $( "#cookie" ).dialog();
+    }
+
 });
